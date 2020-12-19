b68ac108046b954c8615d14567c6389ef2663eee
Joko Sarjono Slameto <johno.smakaduta@gmail.com>
2020-12-19 16:26:28 +0000

    Download source code to /var/www/html/

--- setup_web.sh
+++ setup_web.sh
@@ -9,24 +9,54 @@ then
  sudo apt-get update
  echo "Install webserver"
  sudo apt-get install nginx php-fpm unzip wget mysql-server php-mysql -y
- sudo rm /var/www/html/*
- sudo rm -R /var/www/html/*
+ echo "Delete default html"
+ sudo rm -rf /var/www/html/*
+ #sudo rm  /var/www/html/*
  echo "Download web data"
- sudo wget https://github.com/sdcilsy/sosial-media/archive/master.zip  
- echo "Ekstrak file"
- sudo unzip master.zip
- echo "Memindahkan data"
- sudo mv sosial-media-master/* /var/www/html/
+ cd /var/www/html/ && sudo wget https://github.com/sdcilsy/sosial-media/archive/master.zip && sudo unzip master.zip && sudo mv sosial-media-master/* /var/www/html/
+# echo "Memindahkan data"
+# sudo mv sosial-media-master/* /var/www/html/
  echo "Config file"
  sudo unlink /etc/nginx/sites-enabled/default
- sudo unlink /etc/nginx/sites-enabled/pesbuk
- sudo cp pesbuk /etc/nginx/sites-available/ 
+ sudo unlink /etc/nginx/sites-enabled/pesbuk 
+#sudo tee /etc/nginx/sites-available/pesbuk <<EOF
+#server {
+#        listen 80;
+#        root /var/www/html;
+        # Add index.php to the list if you are using PHP
+#        index index.php index.html index.htm index.nginx-debian.html;
+#        server_name 192.168.43.190;
+        #bisa diganti dengan ip address localhostmu atau ip servermu, nanti kalau sudah ada domain diganti nama domainmu. 
+#        location / {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+#                try_files $uri $uri/ =404;
+#        }
+#        location ~ \.php$ {
+#          include snippets/fastcgi-php.conf;
+#          fastcgi_pass unix:/run/php/php7.2-fpm.sock;
+#        }
+#}
+#EOF
+ sudo cp /home/vagrant/bash-example/pesbuk /etc/nginx/sites-available/ 
  sudo ln -s /etc/nginx/sites-available/pesbuk /etc/nginx/sites-enabled/pesbuk
+# echo "Import database"
+ #sudo mysql -uroot -e "CREATE DATABASE 'dbsosmed' DEFAULT CHARACTER SET utf8 */;"
+ #sudo mysql -uroot -e "CREATE USER 'devopscilsy' IDENTIFIED BY '1234567890';"
+ #sudo mysql -uroot -e "FLUSH PRIVILEGES;"
+# sudo mysql -uroot  << EOF
+#CREATE DATABASE dbsosmed;
+#CREATE USER 'devopscilsy' IDENTIFIED BY '1234567890';
+#GRANT ALL PRIVILEGES ON dbsosmed.* TO devopscilsy@'localhost' IDENTIFIED BY '1234567890';
+#FLUSH PRIVILEGES;
+#EOF
+ #sudo mysql -uroot mysql < /var/www/html/dump.sql
  echo "Enable service"
+ sudo nginx -t
  sudo systemctl restart nginx
  sudo systemctl enable nginx
  echo "Setup selesai"
- exit 0
+# exit 0
 else
  echo "Setup dibatalkan"
  exit 1
