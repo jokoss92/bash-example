63cedfa43ec6f9f61dfab286dc5330e8ebca39fd
Ubuntu <ubuntu@ip-172-31-33-132.ap-southeast-1.compute.internal>
2020-12-30 08:35:19 +0000

    Add config database

--- setup_web.sh
+++ setup_web.sh
@@ -8,53 +8,29 @@ then
  echo "Update repository"
  sudo apt-get update
  echo "Install webserver"
- sudo apt-get install nginx php-fpm unzip wget mysql-server php-mysql -y
+ sudo apt-get install nginx php-fpm unzip wget mysql-server php-mysqli -y
  echo "Delete default html"
  sudo rm -rf /var/www/html/*
- #sudo rm  /var/www/html/*
  echo "Download web data"
- cd /var/www/html/ && sudo wget https://github.com/sdcilsy/sosial-media/archive/master.zip && sudo unzip master.zip && sudo mv sosial-media-master/* /var/www/html/
-# echo "Memindahkan data"
-# sudo mv sosial-media-master/* /var/www/html/
+ cd /var/www/html/ && sudo git clone https://github.com/jokoss92/sosial-media && 
+ ##sudo wget https://github.com/sdcilsy/sosial-media/archive/master.zip && sudo unzip master.zip && 
+ sudo mv sosial-media/* /var/www/html/
  echo "Config file"
  sudo unlink /etc/nginx/sites-enabled/default
  sudo unlink /etc/nginx/sites-enabled/pesbuk 
-#sudo tee /etc/nginx/sites-available/pesbuk <<EOF
-#server {
-#        listen 80;
-#        root /var/www/html;
-        # Add index.php to the list if you are using PHP
-#        index index.php index.html index.htm index.nginx-debian.html;
-#        server_name 192.168.43.190;
-        #bisa diganti dengan ip address localhostmu atau ip servermu, nanti kalau sudah ada domain diganti nama domainmu. 
-#        location / {
-                # First attempt to serve request as file, then
-                # as directory, then fall back to displaying a 404.
-#                try_files $uri $uri/ =404;
-#        }
-#        location ~ \.php$ {
-#          include snippets/fastcgi-php.conf;
-#          fastcgi_pass unix:/run/php/php7.2-fpm.sock;
-#        }
-#}
-#EOF
- sudo cp /home/vagrant/bash-example/pesbuk /etc/nginx/sites-available/ 
+ sudo cp ~/bash-example/pesbuk /etc/nginx/sites-available/ 
  sudo ln -s /etc/nginx/sites-available/pesbuk /etc/nginx/sites-enabled/pesbuk
-# echo "Import database"
- #sudo mysql -uroot -e "CREATE DATABASE 'dbsosmed' DEFAULT CHARACTER SET utf8 */;"
- #sudo mysql -uroot -e "CREATE USER 'devopscilsy' IDENTIFIED BY '1234567890';"
- #sudo mysql -uroot -e "FLUSH PRIVILEGES;"
-# sudo mysql -uroot  << EOF
-#CREATE DATABASE dbsosmed;
-#CREATE USER 'devopscilsy' IDENTIFIED BY '1234567890';
-#GRANT ALL PRIVILEGES ON dbsosmed.* TO devopscilsy@'localhost' IDENTIFIED BY '1234567890';
-#FLUSH PRIVILEGES;
-#EOF
- #sudo mysql -uroot mysql < /var/www/html/dump.sql
  echo "Enable service"
  sudo nginx -t
  sudo systemctl restart nginx
  sudo systemctl enable nginx
+ echo "Setting db"
+sudo mysql -u root << EOF
+CREATE DATABASE IF NOT EXISTS dbsosmed;
+CREATE USER IF NOT EXISTS 'devopscilsy'@'localhost' IDENTIFIED BY '1234567890';
+GRANT ALL PRIVILEGES ON * . * TO 'devopscilsy'@'localhost';
+FLUSH PRIVILEGES;
+EOF
  echo "Setup selesai"
 # exit 0
 else
