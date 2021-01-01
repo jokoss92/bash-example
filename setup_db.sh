fb6eee0db12fdae3d392b28b186ddb18c91b1a2d
Ubuntu <ubuntu@ip-172-31-38-113.ap-southeast-1.compute.internal>
2021-01-01 11:36:04 +0000

    Add script install db server

---
+++ setup_db.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+jawaban="Y"
+
+read -p "Apakah kamu yakin untuk menginstall dbserver ? (Y/n) " pilih;
+if  [ $pilih == $jawaban ];
+then
+ echo "Update repository"
+ sudo apt-get update
+ echo "Install webserver"
+ sudo apt-get install mysql-server -y
+ echo "Setting db"
+sudo mysql -u root << EOF
+CREATE DATABASE IF NOT EXISTS dbsosmed;
+CREATE USER IF NOT EXISTS 'devopscilsy'@'%' IDENTIFIED BY '1234567890';
+GRANT ALL PRIVILEGES ON * . * TO 'devopscilsy'@'%';
+FLUSH PRIVILEGES;
+EOF
+ echo "Import db"
+ mysql -udevopscilsy -p1234567890 dbsosmed < ~/bash-example/dump.sql
+ echo "Restart db service"
+ sudo systemctl enable mysql && sudo systemctl restart mysql && sudo systemctl status mysql 
+ echo "Setup selesai"
+#exit 0
+else
+ echo "Setup dibatalkan"
+#exit 1
+fi
+
+
