3b2166afe4ebc646476455177e2edb0d54e83b7f
Ubuntu <ubuntu@ip-172-31-33-132.ap-southeast-1.compute.internal>
2021-01-01 10:49:24 +0000

    Add git workflow

--- setup_web.sh
+++ setup_web.sh
@@ -7,14 +7,17 @@ if  [ $pilih == $jawaban ];
 then
  echo "Update repository"
  sudo apt-get update
- echo "Install webserver"
- sudo apt-get install nginx php-fpm unzip wget mysql-server php-mysqli -y
+ echo "Install webserver" 
+ sudo apt-get install nginx php-fpm unzip wget php-mysqli -y
  echo "Delete default html"
  sudo rm -rf /var/www/html/*
  echo "Download web data"
- cd /var/www/html/ && sudo git clone https://github.com/jokoss92/sosial-media && 
- ##sudo wget https://github.com/sdcilsy/sosial-media/archive/master.zip && sudo unzip master.zip && 
- sudo mv sosial-media/* /var/www/html/
+ cd /var/www/html/ && sudo git clone https://github.com/jokoss92/sosial-media.git && 
+ sudo mv sosial-media/* /var/www/html/ && sudo mv sosial-media/.[!.]* /var/www/html/
+ sudo sed -i 's/$db_host = "localhost";/$db_host = "172.31.16.217";/g' /var/www/html/config.php 
+ echo "Set git workflow"
+ sudo git remote add upstream https://github.com/sdcilsy/sosial-media.git && sudo git fetch upstream && 
+ sudo git rebase upstream/master && sudo git pull origin master  
  echo "Config file"
  sudo unlink /etc/nginx/sites-enabled/default
  sudo unlink /etc/nginx/sites-enabled/pesbuk 
@@ -24,15 +27,8 @@ then
  sudo nginx -t
  sudo systemctl restart nginx
  sudo systemctl enable nginx
- echo "Setting db"
-sudo mysql -u root << EOF
-CREATE DATABASE IF NOT EXISTS dbsosmed;
-CREATE USER IF NOT EXISTS 'devopscilsy'@'localhost' IDENTIFIED BY '1234567890';
-GRANT ALL PRIVILEGES ON * . * TO 'devopscilsy'@'localhost';
-FLUSH PRIVILEGES;
-EOF
  echo "Setup selesai"
-# exit 0
+#exit 0
 else
  echo "Setup dibatalkan"
  exit 1
